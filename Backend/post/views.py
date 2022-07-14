07a7aebcb7eb65094e403f313517524b00375920
Reny Reyes <renyreyes8@hotmail.com>
2022-07-14 14:51:10 -0400

    Added urls folder + Created get all function

--- Backend/post/views.py
+++ Backend/post/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from rest_framework import status
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# GET ALL POST
+# CREATE A POST
+# LIKE AND DISLIKE POST 
+@api_view(['GET','POST'])
+def post_list(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many = True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
